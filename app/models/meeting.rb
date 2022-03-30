56205eed4291d513abfdee132d7fd97bcb3bf79f
Brandon Lin <bwlin93@gmail.com>
2022-03-30 10:45:53 -0500

    created speakers/meetings/meetingspeakers in local database via console to test

--- app/models/meeting.rb
+++ app/models/meeting.rb
@@ -1,4 +1,8 @@
 class Meeting < ApplicationRecord
-  has_many :meetingspeakers 
-  has_many :speakers, through: :meetingspeakers 
+  has_many :meetingspeakers
+  has_many :speakers, through: :meetingspeakers
+  validates :title, presence: true
+  validates :agenda, presence: true
+  validates :location, presence: true
+  validates :time, presence: true
 end
