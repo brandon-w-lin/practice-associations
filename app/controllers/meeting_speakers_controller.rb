64fe02319fab5940c75e2add0835aedd64fa68ae
Brandon Lin <bwlin93@gmail.com>
2022-03-30 10:33:19 -0500

    creates controllers for meetings, speakers, and meetingspeakers. Gives associations to join tables.

---
+++ app/controllers/meeting_speakers_controller.rb
@@ -0,0 +1,2 @@
+class MeetingSpeakersController < ApplicationController
+end
--- app/models/speaker.rb
+++ app/models/speaker.rb
@@ -1,2 +1,4 @@
 class Speaker < ApplicationRecord
+  has_many :meetingspeakers
+  has_many :meetings, through: :meetingspeakers
 end
